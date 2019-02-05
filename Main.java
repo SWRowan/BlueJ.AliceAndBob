498a5ef63b40d5932acb4f0301bbabef5c2a4d16
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-05 16:26:36 -0500

    Completed Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
+    
     public static void main(String[] args ){
-
+        
+        Scanner names = new Scanner(System.in);
+        System.out.print("Hello, what is your name? ");
+        String name = names.nextLine();
+        
+        if(name == "Bob" || name == "Alice"){
+            System.out.println("Welcome to ZipCode Wilmington " + name + " !");
+        } else { 
+            System.out.println("Better luck next time");
     }
+  }
 }
